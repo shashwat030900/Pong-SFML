72aa78e4149574256d7b009b179e93c05b2c24e9
shashwat030900 <3dartist.shashwat@gmail.com>
2025-05-19 14:58:12 +0530

    Sprite_rendered

--- Header/Gameplay/Ball/Ball.h
+++ Header/Gameplay/Ball/Ball.h
@@ -1,13 +1,22 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
 
 namespace Gameplay {
 	class Ball {
 	private:
+		Texture pong_ball_texture;
+		Sprite pong_ball_sprite;
+		const string texture_path = "Assets/Textures/Ball.png";
 		sf::CircleShape ball_sprite;
-		const float radius = 10.0f;
+		const float scale_x = 0.06f;
+		const float scale_y = 0.06f;
 		const float position_x = 960.0f;
 		const float position_y = 540.0f;
+
+		void loadTexture();
+		void initializeVariables();
 		
 	public:
 		Ball();
--- Source/Gameplay/Ball/Ball.cpp
+++ Source/Gameplay/Ball/Ball.cpp
@@ -2,16 +2,26 @@
 #include "../../Header/Gameplay/Ball/Ball.h"
 
 using namespace Gameplay;
-
+using namespace sf;
 Ball::Ball() {
-	ball_sprite.setRadius(radius);
-	ball_sprite.setPosition(position_x, position_y);
+	loadTexture();
+	initializeVariables();
 }
 
 void Ball::render(sf::RenderWindow* game_window) {
-	game_window->draw(ball_sprite);
+	game_window->draw(pong_ball_sprite);
 }
 
 void Ball::update() {
 	
+}
+void Ball::loadTexture() {
+	if (!pong_ball_texture.loadFromFile(texture_path)) {
+		throw std::runtime_error("Failed to load ball texture!");
+	}
+}
+void Ball::initializeVariables() {
+	pong_ball_sprite.setTexture(pong_ball_texture);
+	pong_ball_sprite.setScale(scale_x, scale_y);
+	pong_ball_sprite.setPosition(position_x, position_y);
 }
